5713f343afd5e10ec240f64cc3d9b908016aa0eb
racesur <cesurraul@gmail.com>
2023-04-14 14:00:46 +0200

    Inicio de Proyecto

---
+++ views/layout.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AutoMotor - Concesionario</title>
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;700;900&display=swap" rel="stylesheet">
+</head>
+<body>
+            
+</body>
+</html>
